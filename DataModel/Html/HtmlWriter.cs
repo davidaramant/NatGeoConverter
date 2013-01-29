654c46fa6cdb0f17ed5cb15d57afe60500882f3d
davidaramant <davidaramant@gmail.com>
2013-01-28 22:00:13 -0500

    Rudimentary beginnings of HTML generator

---
+++ DataModel/Html/HtmlWriter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel.Html {
+    public sealed class HtmlWriter : ITagWriter {
+        readonly StringWriter _writer;
+
+        public HtmlWriter( StringWriter writer ) {
+            _writer = writer;
+
+            _writer.WriteLine( "<html>" );
+        }
+
+        public void Dispose() {
+            _writer.WriteLine( "</html>" );
+        }
+
+        public void Tag( string tag, string content ) {
+            _writer.WriteLine( String.Format( "<{0}>{1}</{0}>", tag, content ) );
+        }
+
+        public void WriteLine( string line ) {
+            _writer.WriteLine( line );
+        }
+
+        public ITagWriter SubTag( string tag ) {
+            return new TagWriter( parent: this, name: tag );
+        }
+    }
+
+    public sealed class TagWriter : ITagWriter {
+        private readonly ITagWriter _parent;
+        private readonly string _name;
+
+        public TagWriter( ITagWriter parent, string name ) {
+            _parent = parent;
+            _name = name;
+
+            _parent.WriteLine( "<" + name + ">" );
+        }
+
+        public void Tag( string tag, string content ) {
+            _parent.WriteLine( String.Format( "<{0}>{1}</{0}>", tag, content ) );
+        }
+
+        public void WriteLine( string line ) {
+            _parent.WriteLine( line );
+        }
+
+        public ITagWriter SubTag( string tag ) {
+            return new TagWriter( parent: this, name: tag );
+        }
+
+        public void Dispose() {
+            _parent.WriteLine( "</" + _name + ">" );
+        }
+    }
+
+    public interface ITagWriter : IDisposable {
+        ITagWriter SubTag( string tag );
+        void Tag( string tag, string content );
+        void WriteLine( string line );
+    }
+
+}
--- Website Generator/Program.cs
+++ Website Generator/Program.cs
@@ -1,17 +1,15 @@
-﻿using System;
+﻿using DataModel;
+using DataModel.Html;
+using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.IO;
-using System.Text;
-using System.Threading.Tasks;
-using DataModel;
+using System.Linq;
 
 namespace Website_Generator {
     class Program {
         readonly static string _basePath = Path.Combine( "G:", "National Geographic" );
         readonly static string _baseJpgPath = Path.Combine( _basePath, "JPG" );
-
+        readonly static string _baseHtmlPath = Path.Combine( _basePath, "HTML" );
 
         static void Main( string[] args ) {
             try {
@@ -21,6 +19,7 @@ namespace Website_Generator {
                 WL( e.ToString() );
             }
 
+            WL( "Done" );
             Console.ReadKey();
         }
 
@@ -37,15 +36,28 @@ namespace Website_Generator {
 
             WL( "{0} decades", decades.Length );
 
-            foreach( var decade in decades ) {
-                foreach( var issue in decade ) {
-                    foreach( var page in issue ) {
-                        if( page.Failed ) {
-                            WL( page.ToString() );
-                        }
-                    }
+            GenerateMainIndex( decades );
+
+
+
+        }
+
+        static void GenerateMainIndex( IEnumerable<NGDecade> decades ) {
+            var sw = new StringWriter();
+
+            using( var index = new HtmlWriter( sw ) ) {
+                using( var head = index.SubTag( "head" ) ) {
+                    head.Tag( "title", "National Geographic" );
+                }
+                using( var body = index.SubTag( "body" ) ) {
+                    body.Tag( "h1", "Hello" );
+                    body.Tag( "p", "World" );
                 }
             }
+
+            File.WriteAllText( Path.Combine( _basePath, "index.html" ), sw.ToString() );
         }
+
+
     }
 }
