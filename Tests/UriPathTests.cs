94be550ac4bc86b3ddc7f7088bf24b61f4c3408f
David Aramant <davidaramant@gmail.com>
2014-03-29 19:54:50 -0400

    Added utility class to correctly generate a URL like Path.Combine does

---
+++ Tests/UriPathTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using Utilities;
+
+namespace Tests {
+	[TestFixture]
+	public class UriPathTests {
+		[Test]
+		public void ShouldCombineUrlComponents() {
+			Assert.That( UriPath.Combine( "html", "1880s", "index.html" ),
+				Is.EqualTo( "html/1880s/index.html" ),
+				"Did not combine paths." );
+		}
+	}
+}
+
---
+++ Utilities/UriPath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Utilities {
+	public static class UriPath {
+		public static string Combine( params string[] components )
+		{
+			var unixPath = Path.DirectorySeparatorChar == '/';
+
+			var url = Path.Combine( components );
+
+			if( !unixPath ) {
+				url = url.Replace( Path.DirectorySeparatorChar, '/' );
+			}
+
+			return url;
+		}
+	}
+}
+
