4f078264692bdd607e62befa4eb3005f4faab2d9
David Aramant <davidaramant@gmail.com>
2014-04-02 21:31:43 -0400

    Added thumbnail template

---
+++ Website Generator/Templates/ThumbnailModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Website_Generator {
+	public class ThumbnailModel {
+		public string LinkUrl { get; private set; }
+
+		public string Description { get; private set; }
+
+		public string ImgUrl { get; private set; }
+
+		public int ImgWidth { get; private set; }
+
+		public int ImgHeight { get; private set; }
+
+		public string ImgAltText { get; private set; }
+
+		public ThumbnailModel(
+			string linkUrl,
+			string description,
+			string imgUrl,
+			int imgWidth,
+			int imgHeight,
+			string imgAltText ) {
+			LinkUrl = linkUrl;
+			Description = description;
+			ImgUrl = imgUrl;
+			ImgWidth = imgWidth;
+			ImgHeight = imgHeight;
+			ImgAltText = imgAltText;
+		}
+	}
+}
