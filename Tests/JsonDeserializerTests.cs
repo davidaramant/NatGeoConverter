850b0348415f1d1bf4f5815cc887e190d1af41e2
David Aramant <davidaramant@gmail.com>
2014-04-18 17:14:05 -0400

    Added JSON deserializer

---
+++ Tests/JsonDeserializerTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using Utilities;
+
+namespace Tests {
+	[TestFixture]
+	public sealed class JsonDeserializerTests {
+		sealed class Test {
+			public string Name { get; set; }
+		}
+
+		[Test]
+		public void ShouldDeserializeJson() {
+			var json = @"{ ""Name"": ""Dog"" }";
+
+			var deserialized = JsonDeserializer.Deserialise<Test>( json );
+
+			Assert.That( deserialized, Is.Not.Null, "Did not return an object." );
+			Assert.That( deserialized.Name, Is.EqualTo( "Dog" ), "Did not read object properties." );
+		}
+	}
+}
+
